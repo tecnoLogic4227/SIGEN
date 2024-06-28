dfdb140663eefca10fd6e449ffc80bc93c3c387b
tecnoLogic4227 <TecnoLogic4227@gmail.com>
2024-06-28 13:53:32 -0300

    Add files via upload

---
+++ PaginaActiveMotion/script/script.js
@@ -0,0 +1,99 @@
+/*..................................................MENU OCULTO.........................................................*/
+$(document).ready(function(){
+    $(".menu-barras").click(function(){
+    $(".menu-adaptable").slideToggle("slow");
+});
+});
+
+$(document).ready(function(){
+$(".op-menu-oculto").click(function(){
+    $(".op-menu-adaptable").hide();
+});
+});
+
+/*.................................................CARRUSEL DE IMAGENES.....................................................*/
+document.addEventListener('DOMContentLoaded', function() {
+  var myCarousel = document.querySelector('#carousel')
+  if (myCarousel) {
+    var carousel = new bootstrap.Carousel(myCarousel, {
+      interval: 3000,
+      wrap: true
+    });
+
+   
+    var prevButton = document.querySelector('.carousel-control-prev');
+    var nextButton = document.querySelector('.carousel-control-next');
+
+    if (prevButton) {
+      prevButton.addEventListener('click', function() {
+        carousel.prev();
+      });
+    }
+
+    if (nextButton) {
+      nextButton.addEventListener('click', function() {
+        carousel.next();
+      });
+    }
+  } else {
+    console.error('Carousel element not found');
+  }
+});
+
+/*...................................................Lista LOCALES.........................................................*/
+
+function toggleAccordion(header) {
+  const item = header.parentElement;
+  const content = item.querySelector('.accordion-content');
+  const arrow = header.querySelector('.arrow');
+  
+  if (content.style.display === 'block') {
+      content.style.display = 'none';
+      arrow.classList.remove('active');
+      item.classList.remove('active');
+  } else {
+      content.style.display = 'block';
+      arrow.classList.add('active');
+      item.classList.add('active');
+  }
+}
+
+/*..........................................BOTON Registro-Login.....................................................*/
+const btnRegistrarse = document.getElementById('btnRegistrarse');
+    btnRegistrarse.addEventListener('click', function() { 
+        window.location.href = 'registro.html';
+    });
+const btnLogin = document.getElementById('btnLogin');
+    btnLogin.addEventListener('click', function() { 
+        window.location.href = 'indexUsuario.html';
+    });
+
+/*...............................................Carrusel Usuario......................................................*/
+document.addEventListener('DOMContentLoaded', function() {
+  var myCarousel = document.querySelector('#carouselExampleIndicators');
+  
+  if (myCarousel) {
+      var carousel = new bootstrap.Carousel(myCarousel, {
+          interval: 3000,
+          wrap: true
+      });
+
+      var prevButton = document.querySelector('.carousel-control-prev');
+      var nextButton = document.querySelector('.carousel-control-next');
+
+      if (prevButton) {
+          prevButton.addEventListener('click', function() {
+              carousel.prev();
+          });
+      }
+
+      if (nextButton) {
+          nextButton.addEventListener('click', function() {
+              carousel.next();
+          });
+      }
+  } else {
+      console.error('Carousel element not found');
+  }
+});
+
